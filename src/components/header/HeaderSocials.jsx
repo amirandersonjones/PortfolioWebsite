a750d6e73651c0b89dafff1eef33d8c27be7c66e
amirandersonjones <amirandersonjones@gmail.com>
2022-04-20 10:07:29 -0500

    workingonnav

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
+import {FaFreeCodeCamp} from 'react-icons/fa'
+
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
     <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
     <a href="https://github.com" target="_blank"><FaGithub /></a>
-    <a href="https://freecodecamp.org" target="_blank"></a>
+    <a href="https://freecodecamp.org" target="_blank"><FaFreeCodeCamp /></a>
     </div>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './nav.css'
+import {FaHome} from 'react-icons/fa'
+import {FaUserTie} from 'react-icons/fa' 
+import {GrUserWorker} from 'react-icons/gr'
+import {RiServiceLine} from 'react-icons/ri'
+import {MdOutlineContactPhone} from 'react-icons/md'
+/*use state hook.home link is automatically active want this to change 
+based on the links that are clicked */
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+     <a href="#" className={activeNav === '#' ? 'active' : ''}><FaHome /></a>
+     <a href="#about"><FaUserTie /></a>
+     <a href="#experience"><GrUserWorker /></a>
+     <a href="#services"><RiServiceLine /></a>
+     <a href="#contact"><MdOutlineContactPhone /></a>
+    </nav>
   )
 }
 

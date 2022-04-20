02caf6d7565a08ebd0d58f24ecaae778e2b1491d
amirandersonjones <amirandersonjones@gmail.com>
2022-04-20 08:35:21 -0500

    more

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/ME.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -10,6 +12,14 @@ const Header = () => {
         <h1> Amir-Anderson Jones</h1>
         <div className="h5 text-light"> Front-End Developer</div>
         <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src= {ME} alt ="me" />
+        </div>
+        
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
       </div>
     </header>
   )

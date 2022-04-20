bce51c1a96b2bc183259dd79d25dd8b4814a378b
amirandersonjones <amirandersonjones@gmail.com>
2022-04-19 19:52:42 -0500

    CV

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I'm</h5>
+        <h1> Amir-Anderson Jones</h1>
+        <div className="h5 text-light"> Front-End Developer</div>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
